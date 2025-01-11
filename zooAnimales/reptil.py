3d362dbdcdba769e985a30c81057c1e2797e5f4b
JackelinBlanquiceth <glozanob@unal.edu.co>
2025-01-10 23:07:15 -0500

    .

--- zooAnimales/reptil.py
+++ zooAnimales/reptil.py
@@ -1,44 +1,44 @@
 from animal import Animal
 class Reptil(Animal):
-    iguanas = 0
-    serpientes = 0
-    _listado = []
-    def __init__(self,nombre, edad, habitat, genero, colorEscamas, largoCola):
-        super().__init__(nombre, edad, habitat, genero)
-        self._colorEscamas = colorEscamas
-        Reptil._listado.append(self)
-        self._largoCola = largoCola
+   iguanas = 0
+   serpientes = 0
+   _listado = []
+   def __init__(self,nombre, edad, habitat, genero, colorEscamas, largoCola):
+      super().__init__(nombre, edad, habitat, genero)
+      self._colorEscamas = colorEscamas
+      Reptil._listado.append(self)
+      self._largoCola = largoCola
 
-    def getListado(self):
-       return self._listado
-    
-    def setListado(self, reptil):
-         self._listado = reptil 
+   def getListado(self):
+      return self._listado
+   
+   def setListado(self, reptil):
+      self._listado = reptil 
 
-    def getColorEscamas(self):
-       return self._colorEscamas
-    
-    def setColorEscamas(self, color):
-       self._colorEscamas = color
+   def getColorEscamas(self):
+      return self._colorEscamas
+   
+   def setColorEscamas(self, color):
+      self._colorEscamas = color
 
-    def getLargoCola(self):
-       return self._largoCola
-    
-    def setLargoCola(self, largo):
-       self._largoCola = largo
-    @classmethod
-    def cantidadReptiles(cls):
-       return len(cls._listado)
-    @classmethod
-    def crearIguana(cls, nombre, edad, genero):
+   def getLargoCola(self):
+      return self._largoCola
+   
+   def setLargoCola(self, largo):
+      self._largoCola = largo
+   @classmethod
+   def cantidadReptiles(cls):
+      return len(cls._listado)
+   @classmethod
+   def crearIguana(cls, nombre, edad, genero):
       Reptil.iguanas += 1
       iguana = Reptil(nombre, edad, "humedal", genero, "verde", 3 )
       return iguana
-    @classmethod
-    def crearSerpiente(cls, nombre, edad, genero):
+   @classmethod
+   def crearSerpiente(cls, nombre, edad, genero):
       Reptil.serpientes += 1
       serpiente = Reptil(nombre, edad, "jungla", genero, "blanco", 1 )
       return serpiente
-       
-    def movimiento(self):
-        return "reptar"
+      
+   def movimiento(self):
+      return "reptar"
