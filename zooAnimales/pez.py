343864d6e2d76ffdd420651a7bc62b2ecd1f9c3b
JackelinBlanquiceth <glozanob@unal.edu.co>
2025-01-12 20:51:13 -0500

    .

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -3,7 +3,7 @@ class Pez(Animal):
     salmones = 0
     bacalaos = 0
     _listado = []
-    def __init__(self, nombre=None, edad=None, habitat=None, genero=None, zona=None, colorEscamas=None, cantidadAletas=None):
+    def __init__(self, nombre, edad, habitat, genero, zona, colorEscamas=None, cantidadAletas=None):
         super().__init__(nombre, edad, habitat, genero, zona)
         Pez._listado.append(self)
         self._colorEscamas = colorEscamas
@@ -29,7 +29,7 @@ class Pez(Animal):
     @classmethod
     def cantidadPeces(cls):
         return len(cls._listado)
-    @staticmethod
+    
     def movimiento(self):
         return "nadar"
     @classmethod
