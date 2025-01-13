e7a1c96fed526fdb5aef020bd619d0b8f93e7fd8
JackelinBlanquiceth <glozanob@unal.edu.co>
2025-01-12 20:54:15 -0500

    Update pez.py

--- zooAnimales/pez.py
+++ zooAnimales/pez.py
@@ -1,39 +1,48 @@
 from zooAnimales.animal import Animal
-
 class Pez(Animal):
     salmones = 0
     bacalaos = 0
     _listado = []
-    def __init__(self, nombre, edad, habitat, genero, colorEscamas =None, cantAletas =None):
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas=None, cantidadAletas=None):
         super().__init__(nombre, edad, habitat, genero)
         Pez._listado.append(self)
         self._colorEscamas = colorEscamas
-        self._cantidadAletas =cantAletas
+        self._cantidadAletas = cantidadAletas
 
+    def getColorEscamas(self):
+        return self._colorEscamas
+    
+    def setColorEscamas(self, color):
+        self._colorEscamas = color
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+    @classmethod
+    def setListado(self, peces):
+        self._listado = peces    
+
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+    
+    def setCantidadAletas(self, cantidad):
+        self._cantidadAletas = cantidad
     @classmethod
     def cantidadPeces(cls):
         return len(cls._listado)
+    
+    def movimiento(self):
+        return "nadar"
     @classmethod
-    def crearSalmon(self, nombre, edad, genero):
-        Pez.salmones +=1
-        salmon = Pez(nombre, edad, "oceano", genero, "rojo", 6)
+    def crearSalmon(cls, nombre, edad, genero):
+        Pez.salmones += 1
+        salmon = Pez(nombre, edad,"oceano", genero, "rojo", 6)
         return salmon
     @classmethod
-    def crearBacalao(self, nombre, edad, genero):
-        Pez.bacalaos +=1
-        bacalao = Pez(nombre, edad, "oceano", genero, "gris", 6)
+    def crearBacalao(cls, nombre, edad, genero):
+        Pez.bacalaos += 1
+        bacalao = Pez(nombre, edad,"oceano", genero, "gris", 6)
         return bacalao
-    def novimiento(self):
-        return "nadar"
-    def getListado(self):
-        return self._listado
-    def setListado(self, list):
-        self._listado = list
-    def getColorEscamas(self):
-        return self._colorEscamas
-    def setColorEscamas(self, colEs):
-        self._colorEscamas =colEs
-    def getCantidadAletas(self):
-        return self._cantidadAletas
-    def setCantidadAletas(self, cantAletas):
-        self._cantidadAletas = cantAletas
+    def toString(self):
+      return f"Mi nombre es {self.getNombre}, tengo una edad de {self.getEdad}, habito en {self.getHabitat} y mi genero es\
+         {self.getGenero}"
+
